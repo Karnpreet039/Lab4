c27bbbf96fdc69c11b2cf08c60fb2bf7fa83d054
Karnpreet Grewal <karnpreetgrewal@Karnpreets-MacBook-Air.local>
2021-05-12 15:34:41 -0700

    Sturctured english

---
+++ lab_4.py
@@ -0,0 +1,12 @@
+"""
+This program allows a user three tries to guess the correct answer to teh question 
+question = "what is teh captital of california". The answer is "Sacramentor"/ 
+
+We first set max_tries = 3. then we create a loop to iterate three times. for each iteration, 
+we ask the user for the anwser (user input).  Then based on the anwser the user gives, 
+we check to see if the user input matches the answer. If so, print "Correct", then terminate the loop with a break statement. 
+
+If the user could not guess the correct answer within the max_tries, then print 
+"You have uded up your allotment of guesses.", then print  "The correct answer is 'California'".
+
+"""
